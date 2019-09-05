46677d454d80b5f3760ee8750854f75e8df7ac14
chase <chase@LAPTOP-3HQQBMAM>
2019-09-05 10:17:14 -0500

    Lab 1 homework

---
+++ src/main/java/CookieCounter.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author chase
+ */
+public class CookieCounter 
+{
+    public static void main(String[] args) 
+    {
+        //create scanner for user input
+        Scanner keyboard = new Scanner(System.in);
+        
+        //asks for users input
+        System.out.println("Please enter the the number of cookies eaten: ");
+        
+        //assign the variable n
+        int n = keyboard.nextInt();
+        
+        //print results
+        System.out.println("The amount of calories eaten was: " + (75*n) + " calories.");
+        
+        
+    }
+}
--- src/main/java/SalesTax.java
+++ src/main/java/SalesTax.java
@@ -3,13 +3,51 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import 
+import java.util.Scanner
 /**
  *
  * @author chase
  */
-public class SalesTax {
-    public static void main(String[] args) {
+public class SalesTax
+{
+    public static void main(String[] args) 
+    {
+        //Constants
+        final float STATE_RATE = 0.04F;
+        final float COUNTY_RATE = 0.02F;
+        
+        //Variables
+        float purchase;
+        float stateTax;
+        float countyTax;
+        float totalTax;
+        float totalCost;
+        
+        //scanner for users input
+        Scanner keyboard = new Scanner(System.in);
+        
+        //get the purchase amount
+        System.out.println("Please enter the purchase amount: ");
+        purchase = (float) keyboard.nextDouble();
+        
+        //calculate the state sales tax
+        stateTax = purchase * STATE_RATE;
+        
+        //calculate county sales tax
+        countyTax = purchase * COUNTY_RATE;
+        
+        //calculate the total sales tax
+        totalTax = stateTax + countyTax;
+        
+        //calculate total purchase tax
+        totalCost = purchase + totalTax;
+        
+        //displaying results of the calculations
+        System.out.println("Purchase amount is: $ " + purchase);
+        System.out.println("State Tax is: $ " + stateTax);
+        System.out.println("CountyTax is: $ " + countyTax);
+        System.out.println("Total tax is: $ " + totalTax);
+        System.out.println("Total Cost is: $ " + totalTax);
         
     }
 }
